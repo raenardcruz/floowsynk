bb3d07b2e8b1ed4dfa9dfc27472ecbeab24cb89c
raenard <raenard.cruz@gmail.com>
2025-01-24 07:29:27 +0800

    Add editor js and mock image

--- app/src/views/Pages.vue
+++ app/src/views/Pages.vue
@@ -1,3 +1,19 @@
 <template>
     <h2>Pages</h2>
+    <div id="editorjs" class="p-3"></div>
 </template>
+
+<script setup>
+import EditorJS from '@editorjs/editorjs';
+import Header from '@editorjs/header'; 
+import List from '@editorjs/list'; 
+
+const editor = new EditorJS({
+    holder: 'editorjs', 
+  
+  tools: { 
+    header: Header, 
+    list: List 
+  },
+});
+</script>
