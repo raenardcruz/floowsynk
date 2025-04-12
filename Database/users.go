e7c2302fc9a4009f94ea886b02bb33800e4d16ae
raenardcruz <raenard.cruz@gmail.com>
2025-04-12 17:59:20 +0800

    cache-aside for users

--- Database/users.go
+++ Database/users.go
@@ -33,6 +33,7 @@ func (db *DatabaseConnection) GetUsers() ([]UsersModel, error) {
 	if err := db.conn.Find(&users).Error; err != nil {
 		return nil, err
 	}
+
 	return users, nil
 }
 
@@ -78,17 +79,33 @@ func (db *DatabaseConnection) AddUser(u UsersModel) error {
 }
 
 func (db *DatabaseConnection) GetUserByUsername(username string) (UsersModel, error) {
+	ctx := context.Background()
+	cacheKey := "user:username:" + username
+
+	if cachedData, found := GetFromCache(ctx, cacheKey); found {
+		var cachedUser UsersModel
+		if err := json.Unmarshal(cachedData, &cachedUser); err == nil {
+			return cachedUser, nil
+		}
+	}
+
 	log.Printf("Getting user with username %s", username)
 	var user UsersModel
 	if err := db.conn.First(&user, "username = ?", username).Error; err != nil {
 		return UsersModel{}, err
 	}
+
+	if err := SetCache(ctx, cacheKey, user, userCacheExpiration); err != nil {
+		log.Printf("Error setting cache for user %s: %v", username, err)
+	}
+
 	return user, nil
 }
 
 func (db *DatabaseConnection) UpdateUser(u UsersModel) error {
 	ctx := context.Background()
 	cacheKey := "user:" + u.ID
+	cacheKeyByUsername := "user:username:" + u.Username
 
 	encPassword, err := crypto.EncryptPassword(u.Password)
 	if err != nil {
@@ -103,6 +120,10 @@ func (db *DatabaseConnection) UpdateUser(u UsersModel) error {
 
 	if err := SetCache(ctx, cacheKey, u, userCacheExpiration); err != nil {
 		log.Printf("Error setting cache for user %s: %v", u.ID, err)
+	}
+
+	if err := SetCache(ctx, cacheKeyByUsername, u, userCacheExpiration); err != nil {
+		log.Printf("Error setting cache for user %s by username: %v", u.Username, err)
 	}
 
 	return nil
