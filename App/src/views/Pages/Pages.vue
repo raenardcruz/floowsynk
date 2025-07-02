af94ba3a09041196d5ea92b40e996837fe6db564
raenardcruz <raenard.cruz@gmail.com>
2025-07-02 22:07:31 +0800

    page builder sidebar contents WIP

--- App/src/views/Pages/Pages.vue
+++ App/src/views/Pages/Pages.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="page-container">
     <Subheader />
+    <div class="page-contents">
+      <PageSidebar style="position: relative;" />
+    </div>
   </div>
 </template>
 
 <script setup>
 import Subheader from '@/components/Page/SubHeader.vue'
+import PageSidebar from '@/components/Page/PageSidebar.vue'
 </script>
 
 <style scoped>
@@ -15,6 +19,14 @@ import Subheader from '@/components/Page/SubHeader.vue'
   flex-direction: column;
   height: 100%;
   width: 100%; 
+  overflow: hidden;
+}
+.page-contents {
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+  width: 100%;
   overflow: hidden;
 }
 </style>
