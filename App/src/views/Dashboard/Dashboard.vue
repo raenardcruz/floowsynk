bc5632fda3cdb1bf687bd44bf54260628f0145a8
Raenard <raenard.cruz@gmail.com>
2025-08-29 13:25:58 +0800

    Remove all unused components. Will create new ones

--- App/src/views/Dashboard/Dashboard.vue
+++ App/src/views/Dashboard/Dashboard.vue
@@ -1,19 +1,62 @@
 <template>
     <div style="padding: 8px">
         <div class="test">
-            <Button label="Test Toast" @click="() => showToast()" />
+            <MenuBar :model="items"/>
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
-import Button from 'primevue/button'
-import { useToast } from 'primevue/usetoast'
+import { ref } from 'vue'
+import MenuBar from 'primevue/menubar'
 
-const toast = useToast()
-const showToast = () => {
-    toast.add({severity:'info', summary: 'Info Message', detail:'Message Content', life: 3000});
-}
+const items = ref([
+    {
+        label: 'Home',
+        icon: 'pi pi-home'
+    },
+    {
+        label: 'Features',
+        icon: 'pi pi-star'
+    },
+    {
+        label: 'Projects',
+        icon: 'pi pi-search',
+        items: [
+            {
+                label: 'Components',
+                icon: 'pi pi-bolt'
+            },
+            {
+                label: 'Blocks',
+                icon: 'pi pi-server'
+            },
+            {
+                label: 'UI Kit',
+                icon: 'pi pi-pencil'
+            },
+            {
+                label: 'Templates',
+                icon: 'pi pi-palette',
+                items: [
+                    {
+                        label: 'Apollo',
+                        icon: 'pi pi-palette'
+                    },
+                    {
+                        label: 'Ultima',
+                        icon: 'pi pi-palette'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        label: 'Contact',
+        icon: 'pi pi-envelope'
+    }
+]);
+
 </script>
 
 <style scoped>
